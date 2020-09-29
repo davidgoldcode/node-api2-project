96be8fc7520d9d52e56c1f13bfcbf77701a2a76e
David Gold <davidgold@hey.com>
2020-09-29 18:28:51 -0400

    added get post by id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Posts = require("../data/db");
 
+// get posts
 router.get("/", (req, res) => {
   Posts.find()
     .then((post) => {
@@ -10,6 +11,23 @@ router.get("/", (req, res) => {
     .catch((err) => {
       res
         .status(500)
+        .json({ message: "The post information could not be retrieved" });
+    });
+});
+
+// get post comments
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((post) => {
+      post
+        ? res.status(200).json(post)
+        : res
+            .status(404)
+            .json({ message: "The post could not be found with that ID" });
+    })
+    .catch((err) => {
+      res
+        .statusCode(500)
         .json({ message: "The post information could not be retrieved" });
     });
 });
