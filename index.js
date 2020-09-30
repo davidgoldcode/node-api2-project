4dd59fd01cab6a1660d42648090db98475f8d17f
David Gold <davidgold@hey.com>
2020-09-29 21:44:04 -0400

    mvp

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const postsRouter = require("./posts/posts-router.js");
-const Posts = require("./data/db");
 
 const server = express();
 
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { findPostComments, remove } = require("../data/db");
 const router = express.Router();
 const Posts = require("../data/db");
 
@@ -47,7 +48,7 @@ router.get("/:id/comments", (req, res) => {
     });
 });
 
-// make POST request to /api/posts
+// make POST request to /api/posts - to revisit
 router.post("/", (req, res) => {
   if (!req.body.contents || !req.body.title) {
     return res.status(400).json({
@@ -57,11 +58,63 @@ router.post("/", (req, res) => {
 
   Posts.insert(req.body)
     .then((post) => {
-      res.status(201).json({ ...post });
+      res.status(201).json({ post: post[0] });
     })
     .catch((err) => {
       res.status(500).json({
         error: "There was an error while saving the post to the database",
+      });
+    });
+});
+
+// post comments - to revisit
+router.post("/:id/comments", (req, res) => {
+  const { text } = req.body;
+  const { id: post_id } = req.params;
+  console.log(req.body.text);
+
+  if (!text) {
+    return res
+      .status(400)
+      .json({ message: "Please provide text for the comment" });
+  }
+
+  Posts.insertComment({ text, post_id })
+    .then((comment) => {
+      console.log("comment", comment);
+      if (!comment.id) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      } else {
+        res.status(201).json(comment);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "There was an error while saving the comment to the database",
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Posts.remove(id)
+    .then((post) => {
+      if (post > 0) {
+        res.status(200).json({
+          message: "The post has been deleted",
+        });
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "The post could not be removed",
       });
     });
 });
